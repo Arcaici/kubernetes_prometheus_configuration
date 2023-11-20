9523aca5fe4f5ecee3687fe1560a450fe500a9fb
Marco Venturi <marco.venturi.97@hotmail.it>
2023-11-20 12:41:30 +0100

    prometheus configuration for scape

--- serviceToScrape/simulation_script.py
+++ serviceToScrape/simulation_script.py
@@ -1,7 +1,22 @@
+import time
+import random
 import requests
 
 web_server_url = "http://localhost:8081"
 
-# Simulate requests
-response = requests.get(f"{web_server_url}/simulate-requests")
-print(response.text)
+
+def main():
+    start_time = time.time()
+    end_time = start_time + 20 * 60  # Run for 20 minutes
+
+    while time.time() < end_time:
+        # Generate a random sleep time between 1 and 5 seconds
+        sleep_time = random.uniform(1, 5)
+        time.sleep(sleep_time)
+
+        # Simulate requests
+        response = requests.get(f"{web_server_url}/simulate-requests")
+        print(response.text)
+
+if __name__ == "__main__":
+    main()
