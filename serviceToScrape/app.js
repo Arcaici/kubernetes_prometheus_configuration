e0472e292e67ade599fcee87133c670ecbae3d97
Marco Venturi <marco.venturi.97@hotmail.it>
2023-11-21 13:27:37 +0100

    monitoring with custom metric working

--- serviceToScrape/app.js
+++ serviceToScrape/app.js
@@ -1,18 +1,58 @@
 const express = require('express');
-const Prometheus = require('prom-client');
+const prometheus = require('prom-client');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-const register = new Prometheus.Registry();
-register.setDefaultLabels({
-  app: 'simulationjs'
+/* Default labels for application metrics*/
+/* and default metrics collection */
+const prometheusRegistry = new prometheus.Registry();
+prometheus.collectDefaultMetrics({ register: prometheusRegistry });
+prometheusRegistry.setDefaultLabels({
+  app: 'simulation'
 })
-Prometheus.collectDefaultMetrics({register})
 
+/* Custom metrics */
+const numberOfRequests = new prometheus.Counter({
+  name: 'simulation_app_requests_total',
+  help: 'Total number of requests to the simulation app',
+  labelNames: ['method'],
+  registers: [prometheusRegistry],
+});
+
+// Add a histogram for request durations
+const requestDurationHistogram = new prometheus.Histogram({
+  name: 'simulation_app_request_duration_seconds',
+  help: 'Histogram of request durations for the simulation app',
+  labelNames: ['route'],
+  registers: [prometheusRegistry],
+  buckets: [0.1, 0.5, 1, 2, 5], // Specify histogram buckets in seconds
+});
+
+
+/* application routes and logic */
+
+app.use((req, res, next) => {
+  numberOfRequests.inc({ method: req.method });
+
+  // Start measuring request duration
+  const end = requestDurationHistogram.startTimer();
+
+  // Attach the histogram timer to the request for use in later middleware
+  req.requestDurationTimer = end;
+
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('Hello, this is your web server!');
+  req.requestDurationTimer();
+});
+
+app.get('/error', (req, res) => {
+  numberOfRequests.inc({ method: 'error' });
+  // Simulate an error
+  throw new Error('Simulated error');
 });
 
 app.get('/simulate-requests', (req, res) => {
@@ -20,15 +60,32 @@ app.get('/simulate-requests', (req, res) => {
   for (let i = 0; i < 5; i++) {
     console.log(`Processing request ${i + 1}`);
   }
-
+  numberOfRequests.inc({ method: 'succeed' });
   res.send('Simulated requests completed!');
+  req.requestDurationTimer();
 });
 
+/* metrics endpoint */
 app.get('/metrics', (req, res) => {
-  res.setHeader('Content-Type',register.contentType)
-  register.metrics().then(data => res.status(200).send(data))
+  res.set('Content-Type', prometheus.register.contentType);
+  prometheusRegistry.metrics().then(data => res.status(200).send(data))
 });
+
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
+// clear shutdown
+process.on('SIGTERM', () => {
+  clearInterval(metricsInterval)
+
+  server.close((err) => {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    }
+
+    process.exit(0)
+  })
+})
--- serviceToScrape/simulation_script.py
+++ serviceToScrape/simulation_script.py
@@ -9,18 +9,20 @@ def instruction():
     # This is the function representing the instruction you want to execute
             # Simulate requests
     time.sleep(random.uniform(0.5 , 1))
-    response = requests.get(f"{web_server_url}/simulate-requests")
+    if round(random.random()) == 0:
+        response = requests.get(f"{web_server_url}/error")
+    else:
+        response = requests.get(f"{web_server_url}/simulation-requests")
     print(response.text)
 
 def main():
-    start_time = time.time()
-    end_time = start_time + (2 * 60)  # Run for 20 minutes
-    print((end_time - start_time)/60)
+    i = 100000
     # Using ThreadPoolExecutor to parallelize the execution
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        while end_time - time.time() > 0:
+        while (i > 0):
             # Submit the instruction function to the executor
             executor.submit(instruction)
+            i = i-1
     print("COMPLETE!!!")
 if __name__ == "__main__":
     main()
